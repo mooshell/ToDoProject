ba5367a3107e5d8126c9aa75594a8f746cc4bcf2
Michelle Elias Flores <michelleeliasflores@gmail.com>
2023-05-20 18:18:10 -0400

    Initial Commit

---
+++ ToDoProject/ToDoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoProjectApp.swift
+//  ToDoProject
+//
+//  Created by Michelle Elias Flores on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
